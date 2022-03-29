fc755899dbfce8d1a9ff6d0eaf0ece39940aa13a
msyamsularif <syamsularif0904@gmail.com>
2022-03-29 13:13:35 +0700

    update notification content on water reminder notification

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -17,7 +17,8 @@ Future<void> createPlantFoodNotification() async {
 }
 
 Future<void> createWaterReminderNotification(
-    NotificationWeekAndTime notificationSchedule) async {
+  NotificationWeekAndTime notificationSchedule,
+) async {
   await AwesomeNotifications().createNotification(
     content: NotificationContent(
       id: createUniqueId(),
@@ -25,6 +26,20 @@ Future<void> createWaterReminderNotification(
       title: '${Emojis.wheater_droplet} Add some water to your plant!',
       body: 'Water your plant regularly to keep it healthy.',
       notificationLayout: NotificationLayout.Default,
+    ),
+    actionButtons: [
+      NotificationActionButton(
+        key: 'MARK_DONE',
+        label: 'Mark Done',
+      ),
+    ],
+    schedule: NotificationCalendar(
+      weekday: notificationSchedule.dayOfTheWeek,
+      hour: notificationSchedule.timeOfDay.hour,
+      minute: notificationSchedule.timeOfDay.minute,
+      second: 0,
+      millisecond: 0,
+      repeats: true,
     ),
   );
 }
