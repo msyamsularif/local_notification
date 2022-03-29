d2be7c45cdc96d8350aa2f55d208373ee3290022
msyamsularif <syamsularif0904@gmail.com>
2022-03-29 10:53:29 +0700

    add awesome notifications initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 
 import 'home_page.dart';
 
 void main() {
+  AwesomeNotifications().initialize(
+    'resource://drawable/res_notification_app_icon',
+    [
+      NotificationChannel(
+        channelKey: 'basic_channel',
+        channelName: 'Basic Notifications',
+        defaultColor: Colors.teal,
+        importance: NotificationImportance.High,
+        channelShowBadge: true,
+        channelDescription: '',
+      ),
+    ],
+  );
   runApp(const AppWidget());
 }
 
